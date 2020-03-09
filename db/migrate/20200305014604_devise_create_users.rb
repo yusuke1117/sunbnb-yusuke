af399572ab7ab095b9cdce24e6be6e699f53521d
yusuke1117 <iysk.iwaisk@gmail.com>
2020-03-09 10:16:04 +0800

    [Function] Email Confirmation

--- db/migrate/20200305014604_devise_create_users.rb
+++ db/migrate/20200305014604_devise_create_users.rb
@@ -22,10 +22,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :current_sign_in_ip
       # t.string   :last_sign_in_ip
 
-      ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
+      # Confirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
       # t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
@@ -39,7 +39,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
+    add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
 end

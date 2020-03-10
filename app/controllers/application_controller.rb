6148835da2fdabf9e28c99e5c1ff2442290cdc25
yusuke1117 <60721154+yusuke1117@users.noreply.github.com>
2020-03-10 14:06:08 +0800

    [Layout] Edit Profile (#6)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone_number, :description])
   end
 end

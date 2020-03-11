047b2b077063dd7693c2b785fd04bb405dbe18bd
yusuke1117 <iysk.iwaisk@gmail.com>
2020-03-11 10:16:05 +0800

    [Layout] User Show Page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def avatar_url(user)
     if user.image
-      "https://graph.facebook.com/#{user.uid}/picture?type=small"
+      "https://graph.facebook.com/#{user.uid}/picture?type=large"
     else
       gravatar_id = Digest::MD5::hexdigest(user.email).downcase
       "https://secure.gravatar.com/avatar/#{gravatar_id}"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  # devise_for :users
 
   devise_for :users, controllers: {
     registrations: 'users/registrations',
@@ -10,5 +9,6 @@ Rails.application.routes.draw do
 
   }
 
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

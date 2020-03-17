a6df2f44990ec96ef257343e8487ddff1e15ffb1
yusuke1117 <iysk.iwaisk@gmail.com>
2020-03-17 09:29:13 +0800

    [Function] Delete Photos with Ajax

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,11 +2,22 @@ class PhotosController < ApplicationController
   def create
     @room = Room.find(params[:room_id])
   
+    # def destroy
+    #   @photo = Photo.find(params[:id])
+    #   @photo.destroy
+      
+    #   redirect_back(fallback_location: request.referer, notice: "Saved...")
+    #   end
+
     def destroy
       @photo = Photo.find(params[:id])
+      room = @photo.room
+
       @photo.destroy
-      
-      redirect_back(fallback_location: request.referer, notice: "Saved...")
+      @photos = Photo.where(room_id: room.id)
+
+      respond_to :js
+
     end
 
     if params[:images]

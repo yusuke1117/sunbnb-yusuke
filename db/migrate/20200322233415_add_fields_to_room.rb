0d2572831dd6071030ed587b46ad71b09aed81f1
yusuke1117 <iysk.iwaisk@gmail.com>
2020-03-23 10:57:11 +0800

    [Layout] google maps

--- db/migrate/20200322233415_add_fields_to_room.rb
+++ db/migrate/20200322233415_add_fields_to_room.rb
@@ -1,7 +1,7 @@
 class AddFieldsToRoom < ActiveRecord::Migration[5.2]
   def change
-    add_column :rooms, :latitube, :float
-    add_column :rooms, :longtitube, :float
+    add_column :rooms, :latitude, :float
+    add_column :rooms, :longitude, :float
 
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -42,8 +42,8 @@ ActiveRecord::Schema.define(version: 2020_03_22_233415) do
     t.boolean "has_air_conditioning"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "latitube"
-    t.float "longtitube"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|

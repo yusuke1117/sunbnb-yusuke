c01166d25489daa631f7e8f880cc303549bfc6ba
yusuke1117 <iysk.iwaisk@gmail.com>
2020-04-17 15:19:28 +0800

    first commit

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# additional code due to heroku deproy error
-config.assets.initialize_on_precompile = false
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -13,6 +11,9 @@ module SunbnbYusuke
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+    # additional code due to heroku deproy error
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers

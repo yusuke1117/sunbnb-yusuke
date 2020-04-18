3675e795e29ddd1f0c1da3fdefb92a23c48fce22
yusuke1117 <iysk.iwaisk@gmail.com>
2020-04-18 16:46:32 +0800

    without bundle install

--- config/application.rb
+++ config/application.rb
@@ -12,9 +12,6 @@ module SunbnbYusuke
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # additional code due to heroku deproy error
-    config.assets.initialize_on_precompile = false
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

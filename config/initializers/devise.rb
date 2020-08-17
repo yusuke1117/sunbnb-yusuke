98757f5528cd196590e25fb921d62dc906fe46e8
yusuke1117 <iysk.iwaisk@gmail.com>
2020-08-17 21:43:19 +0900

    first commit

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -297,6 +297,6 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
-  config.omniauth :facebook, Rails.application.credentials.facebook[219017569293124], Rails.application.credentials.facebook[79dfd2b9a8ebaa66155a97d6f319a738], scope: 'email', info_fields: 'email, name'
+  config.omniauth :facebook, Rails.application.credentials.facebook[219017569293124], Rails.application.credentials.facebook[79dfd2b9a8ebaa66155a97d6f319a738], scope: 'email' info_fields: 'email, name'
 
 end

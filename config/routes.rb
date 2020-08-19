8d4f16a2f73d32c8de0f3167aa566219a6005645
yusuke1117 <iysk.iwaisk@gmail.com>
2020-08-20 01:02:56 +0900

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
-    devise_for :users,
-              path: '',
-              path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
-              controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
+  devise_for :users,
+    path: '',
+    path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
+    controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
 
   resources :rooms do 
     member do

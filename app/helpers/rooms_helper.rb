23fef4020775be4ff8d1f55d170aec481764c96f
yusuke1117 <iysk.iwaisk@gmail.com>
2020-08-31 21:15:21 +0900

    second commit

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,13 +1,9 @@
 module RoomsHelper
-  def  is_done?(item)
-    unless item.blank?
-      content_tag :span, class: "" do
-        content_tag :i, nil, class: "fa fa-check float-right"
-      end
+    def is_done?(item)
+        if !item.blank?
+            content_tag :span, class: "pull-right text-babu" do
+                content_tag :i, nil, class: "fa fa-check"
+            end
+        end
     end
-  end
-
-  def is_not_ready?(room)
-    room.price.blank? || room.name.blank? || room.photos.blank? || room.address.blank?
-  end
 end

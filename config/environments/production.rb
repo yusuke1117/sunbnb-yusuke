0e52132b1dea2ec221502d28312b66dc46bbf521
yusuke1117 <iysk.iwaisk@gmail.com>
2020-09-03 22:31:21 +0900

    second commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,17 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { host: 'https://airbnb-yusuke.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.mailgun.org',
+    port: 587,
+    domain: Rails.application.credentials.mailgun[:domain],
+    authentication: 'plain',
+    user_name: Rails.application.credentials.mailgun[:user_name],
+    password: Rails.application.credentials.mailgun[:password]
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true

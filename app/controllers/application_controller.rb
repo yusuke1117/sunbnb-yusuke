a5267a276628130aad6b3b5fed132d485f3296c0
yusuke1117 <iysk.iwaisk@gmail.com>
2020-09-04 15:14:12 +0900

    second commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   include RoomsHelper
   
+
+  
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
